cab7f44c62fd007206d69e4a8143450f27b5b131
nakanoyuki <yukich41@gmail.com>
2025-04-04 13:53:52 +0900

    todoリスト

--- src/app.ts
+++ src/app.ts
@@ -1,153 +1,47 @@
-interface Validatable {
-  value: string | number;
-  required?: boolean;
-  minLength?: number;
-  maxLength?: number;
-  min?: number;
-  max?: number;
+interface Todo {
+  id: number;
+  text: string;
+  complated: boolean;
 }
 
-function validate(validateInput: Validatable) {
-  let isValid = true;
-  if (validateInput.required) {
-    isValid == isValid && validateInput.value?.toString().trim().length !== 0;
-  }
-  if (validateInput.minLength && typeof validateInput.value == "string") {
-    isValid == isValid && validateInput.value.length >= validateInput.minLength;
-  }
-  if (validateInput.maxLength && typeof validateInput.value == "string") {
-    isValid == isValid && validateInput.value.length <= validateInput.maxLength;
-  }
-  if (validateInput.min && typeof validateInput.value == "number") {
-    isValid = isValid && validateInput.value >= validateInput.min;
-  }
-  if (validateInput.max && typeof validateInput.value == "number") {
-    isValid = isValid && validateInput.value <= validateInput.max;
-  }
-  return isValid;
-}
-class ProjectInput {
-  templateElement: HTMLTemplateElement;
-  hostElement: HTMLDivElement;
-  element: HTMLFormElement;
+const textInput = document.getElementById("todo-input")! as HTMLInputElement;
+const button = document.getElementById("add-btn")! as HTMLButtonElement;
+const listElement = document.getElementById("todo-list")! as HTMLUListElement;
 
-  titleInputElement: HTMLInputElement;
-  descriptionInputElement: HTMLInputElement;
-  mandayInputElement: HTMLInputElement;
+let todos: Todo[] = [];
 
-  constructor() {
-    this.templateElement = document.getElementById(
-      "project-input"
-    )! as HTMLTemplateElement;
-    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+function updateTodo() {
+  listElement.innerHTML = "";
 
-    const importedNode = document.importNode(
-      this.templateElement.content,
-      true
-    );
-    this.element = importedNode.firstElementChild as HTMLFormElement;
-    this.element.id = "user-input";
+  todos.forEach((todo) => {
+    const list = document.createElement("li");
+    list.textContent = todo.text;
 
-    this.titleInputElement = this.element.querySelector(
-      "#title"
-    ) as HTMLInputElement;
-    this.descriptionInputElement = this.element.querySelector(
-      "#description"
-    ) as HTMLInputElement;
-    this.mandayInputElement = this.element.querySelector(
-      "#manday"
-    ) as HTMLInputElement;
+    const deletebutton = document.createElement("button");
+    deletebutton.textContent = "削除";
+    deletebutton.addEventListener("click", () => deleteTodo(todo.id));
 
-    this.configure();
-    this.attach();
-  }
-  private gatherUserInput(): [string, string, number] | void {
-    const enteredTitle = this.titleInputElement.value;
-    const enteredDescription = this.descriptionInputElement.value;
-    const enteredManday = this.mandayInputElement.value;
-    const titleValidatable: Validatable = {
-      value: enteredTitle,
-      required: true,
-    };
-    const descriptionValidatable: Validatable = {
-      value: enteredDescription,
-      required: true,
-      maxLength: 5,
-    };
-    const mandayValidatable: Validatable = {
-      value: +enteredManday,
-      required: true,
-      min: 4,
-      max: 1000,
-    };
-    if (
-      !validate(titleValidatable) ||
-      !validate(descriptionValidatable) ||
-      !validate(mandayValidatable)
-    ) {
-      alert("入力値が正しくありません");
-    } else {
-      return [enteredTitle, enteredDescription, +enteredManday];
-    }
-  }
-
-  private clearInput() {
-    this.titleInputElement.value = "";
-    this.descriptionInputElement.value = "";
-    this.mandayInputElement.value = "";
-  }
-  private submitHandler = (event: Event) => {
-    event.preventDefault();
-    const userInput = this.gatherUserInput();
-    if (Array.isArray(userInput)) {
-      const [title, desc, manday] = userInput;
-      console.log(title, desc, manday);
-      this.clearInput();
-    }
-  };
-
-  private configure() {
-    this.element.addEventListener("submit", this.submitHandler);
-  }
-
-  private attach() {
-    this.hostElement.insertAdjacentElement("afterbegin", this.element);
-  }
+    list.appendChild(deletebutton);
+    listElement.appendChild(list);
+  });
 }
 
-class ProjectList {
-  templateElement: HTMLTemplateElement;
-  hostElement: HTMLDivElement;
-  element: HTMLElement;
-
-  constructor(private type: "active" | "finished") {
-    this.templateElement = document.getElementById(
-      "project-list"
-    )! as HTMLTemplateElement;
-    this.hostElement = document.getElementById("app")! as HTMLDivElement;
-
-    const importedNode = document.importNode(
-      this.templateElement.content,
-      true
-    );
-    this.element = importedNode.firstElementChild as HTMLElement;
-    this.element.id = `${this.type}-projects`;
-    this.attach();
-    this.renderContent();
-  }
-
-  private renderContent() {
-    const listId = `${this.type}-product-id`;
-    this.element.querySelector("ul")!.id = listId;
-    this.element.querySelector("h2")!.textContent =
-      this.type === "active" ? "実行プロジェクト" : "完了プロジェクト";
-  }
-
-  private attach() {
-    this.hostElement.insertAdjacentElement("beforeend", this.element);
-  }
+function addTodo(text: string): void {
+  const newTodo = { id: Date.now(), text, complated: true };
+  todos.push(newTodo);
+  updateTodo();
 }
 
-const prjInput = new ProjectInput();
-const activeProduct = new ProjectList("active")
-const finishedProduct = new ProjectList("finished")
+function deleteTodo(id: number): void {
+  const index = todos.findIndex((todo) => todo.id === id);
+  if (index !== -1) {
+    todos.splice(index, 1);
+  }
+
+  updateTodo();
+}
+
+button.addEventListener("click", () => {
+  addTodo(textInput.value);
+  textInput.value = "";
+});
